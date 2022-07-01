243c37f5c7e3efb948e8824ea0511bb7cddecb6c
itsberriver <jessicamejiaosorio@MacBook-Air-de-JessiJess.local>
2022-07-01 12:59:30 +0200

    deleted the spaces between the labels

--- src/App.js
+++ src/App.js
@@ -23,31 +23,31 @@ function App() {
         <Screen input = { input } />
 
         <div className="row">
-          <Button manageClick={addInput}> 1 </Button>
-          <Button manageClick={addInput}> 2 </Button>
-          <Button manageClick={addInput}> 3 </Button>
-          <Button manageClick={addInput}> + </Button>
+          <Button manageClick={addInput}>1</Button>
+          <Button manageClick={addInput}>2</Button>
+          <Button manageClick={addInput}>3</Button>
+          <Button manageClick={addInput}>+</Button>
         </div>
 
         <div className="row">
-          <Button manageClick={addInput}> 4 </Button>
-          <Button manageClick={addInput}> 5 </Button>
-          <Button manageClick={addInput}> 6 </Button>
-          <Button manageClick={addInput}> - </Button>
+          <Button manageClick={addInput}>4</Button>
+          <Button manageClick={addInput}>5</Button>
+          <Button manageClick={addInput}>6</Button>
+          <Button manageClick={addInput}>-</Button>
           </div>
 
         <div className="row">
-          <Button manageClick={addInput}> 7 </Button>
-          <Button manageClick={addInput}> 8 </Button>
-          <Button manageClick={addInput}> 9 </Button>
-          <Button manageClick={addInput}> * </Button>
+          <Button manageClick={addInput}>7</Button>
+          <Button manageClick={addInput}>8</Button>
+          <Button manageClick={addInput}>9</Button>
+          <Button manageClick={addInput}>*</Button>
           </div>
 
         <div className="row">
           <Button manageClick={calculateResult}> = </Button>
-          <Button manageClick={addInput}> 0 </Button>
-          <Button manageClick={addInput}> . </Button>
-          <Button manageClick={addInput}> / </Button>
+          <Button manageClick={addInput}>0</Button>
+          <Button manageClick={addInput}>.</Button>
+          <Button manageClick={addInput}>/</Button>
           </div>
 
         <div className="row">
